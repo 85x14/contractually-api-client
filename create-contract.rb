9aed46c2b76f0d65587d100a73072f0ba36d2499
Brendan Mulholland <me@bmulholland.ca>
2015-03-10 13:51:51 -0700

    Refactor and split up the API class tructure

--- create-contract.rb
+++ create-contract.rb
@@ -2,56 +2,82 @@ require 'open-uri'
 require 'csv'
 require 'httparty'
 
-class Contractually
-  include HTTParty
+module Contractually
+  class Api
+    include HTTParty
 
+    def initialize(server, api_token)
+      self.class.base_uri "#{server}/v0/"
+      # Empty body required so we don't get a 411 error
+      # See https://github.com/jnunemaker/httparty/issues/124
+      @options = { query: { api_token: api_token }, body: "" }
+    end
 
-  def initialize(server, api_token)
-    self.class.base_uri "#{server}/v0/"
-    # Empty body required so we don't get a 411 error
-    # See https://github.com/jnunemaker/httparty/issues/124
-    @options = { query: { api_token: api_token }, body: "" }
-    #@options[:basic_auth] = {username: "staging", password: "!*pass9"}
+    def query(type, path, data)
+      options = @options.clone
+      options[:query].merge!(data)
+      self.class.send(type, path, options)
+    end
+
+    def post(path, data)
+      self.query(:post, path, data)
+    end
+
+    def put(path, data)
+      self.query(:put, path, data)
+    end
   end
 
-  def create_contract(template_id)
-    options = @options.clone
-    options[:query][:template_id] = template_id
+  class Contract
+    def initialize(api, template_id)
+      @api = api
 
-    response = self.class.post("/contracts", options)
-    raise "Contract not created: #{response.body}" unless response.code == 201
+      data = { template_id: template_id }
 
-    @contract_id = JSON.parse(response.body)["contract_id"]
-    raise "Invalid contract id #{@contract_id}!" unless @contract_id.size == 5
+      response = @api.post("/contracts", data)
+      raise "Contract not created: #{response.body}" unless response.code == 201
 
-    puts "Created contract #{@contract_id}"
+      @contract_id = JSON.parse(response.body)["contract_id"]
+      raise "Invalid contract id #{@contract_id}!" unless @contract_id.length == 5
+
+      puts "Created contract #{@contract_id}"
+    end
+
+    def fill_fields(fields)
+      # Have to URI encode the keys because nothing else does it for us
+      data = { fields: Hash[fields.map{|k, v| [URI::encode(k), v] }] }
+
+      response = @api.put("/contracts/#{@contract_id}/fields", data)
+      raise "Could not fill fields #{fields}: #{response.body}" unless response.code == 204
+
+      puts "Filled fields"
+    end
+
+    def contract_id
+      @contract_id
+    end
+
   end
 
-  def fill_contract_fields(fields)
-    options = @options.clone
+  class Invite
+    def initialize(api, contract, name, email)
+      @api = api
+      @contract_id = contract.contract_id
 
-    # Have to URI encode the keys because nothing else does it for us
-    options[:query][:fields] = Hash[fields.map{|k, v| [URI::encode(k), v] }]
+      data = {
+        party: {
+          organization: "",
+          name: name,
+          email: email,
+          permissions: ["sign", "fill_fields"]
+        }
+      }
 
-    response = self.class.put("/contracts/#{@contract_id}/fields", options)
-    raise "Could not fill fields #{fields}: #{response.body}" unless response.code == 204
+      response = @api.post("/contracts/#{@contract_id}/parties", data)
+      raise "Could not invite user #{name} @ #{email}: #{response.body}" unless response.code == 201
 
-    puts "Filled fields"
-  end
-
-  def invite_user(name, email)
-    options = @options.clone
-    options[:query][:party] = {
-      organization: "",
-      name: name,
-      email: email,
-      permissions: ["sign", "fill_fields"]
-    }
-
-    response = self.class.post("/contracts/#{@contract_id}/parties", options)
-    raise "Could not invite user #{name} @ #{email}: #{response.body}" unless response.code == 201
-
-    puts "Invited user #{name}"
+      puts "Invited user #{name}"
+    end
   end
 end
 
@@ -59,14 +85,21 @@ end
 @api_token = ARGV[1]
 @template_id = ARGV[2]
 @filename = ARGV[3]
+@invite_user = ARGV[4]
 
 CSV.foreach(@filename, headers: true) do |row|
-  contract = Contractually.new(@server, @api_token)
+  api = Contractually::Api.new(@server, @api_token)
 
-  contract.create_contract(@template_id)
+  contract = Contractually::Contract.new(api, @template_id)
 
-  contract.fill_contract_fields(row)
+  contract.fill_fields(row)
 
-  name = "#{row["First Name"]} #{row["Last Name"]}"
-  contract.invite_user(name, row["Email Address"])
+  if @invite_user
+    raise "Must have a First Name column to invite a user" if row["First Name"].nil? || row["First Name"] == ""
+    raise "Must have a Last Name column to invite a user" if row["Last Name"].nil? || row["Last Name"] == ""
+    raise "Must have an Email Address column to invite a user" if row["Email Address"].nil? || row["Email Address"] == ""
+
+    name = "#{row["First Name"]} #{row["Last Name"]}"
+    Contractually::Invite.new(api, contract, name, row["Email Address"])
+  end
 end
